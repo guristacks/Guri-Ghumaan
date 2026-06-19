27449d712d5b3a43abf134d9202cd9d8edd5b5ec
Guri <guristacks@gmail.com>
2026-06-20 02:07:55 +0530

    Enhanced structure and readability

--- src/Components/About-Page/AboutTop.jsx
+++ src/Components/About-Page/AboutTop.jsx
@@ -1,10 +1,20 @@
 import React from "react";
-const AboutTop = ({ styles, helloRef, blackRef }) => {
 
+const AboutTop = ({ styles, helloRef, blackRef }) => {
     return (
         <section className={styles.AboutTopSec}>
             <div ref={blackRef} className={`${styles.black} ${styles.flex} ${styles.ac} ${styles.jc}`}>
                 <h2 ref={helloRef} className={styles.helloText}>• About</h2>
+            </div>
+            <div className={styles.container}>
+                <div className={styles.TopContent}>
+                    <h2>Crafting scalable web experiences. </h2>
+                    <div className={`${styles.TopBtm} ${styles.flex} ${styles.ac} ${styles.sb}`}>
+                        <p>Branding</p>
+                        <p>Design</p>
+                        <p>Development</p>
+                    </div>
+                </div>
             </div>
         </section>
     )
