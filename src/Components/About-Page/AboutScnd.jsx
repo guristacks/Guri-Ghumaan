b050a7e09ba1a80fbdaf03d1697df40926d2ae09
Guri <guristacks@gmail.com>
2026-06-20 02:07:42 +0530

    Add AboutScnd component with stats and brand logos

---
+++ src/Components/About-Page/AboutScnd.jsx
@@ -0,0 +1,106 @@
+import React from "react";
+
+const stats = [
+    {
+        Result: "$74M",
+        Reason: "Driving growth with strategy.",
+    },
+    {
+        Result: "95%",
+        Reason: "Building trusted partnerships.",
+    },
+    {
+        Result: "225+",
+        Reason: "Delivering industry success.",
+    },
+    {
+        Result: "92%",
+        Reason: "Turning traffic into growth.",
+    },
+];
+
+const brands = [
+    {
+        LogoImg:
+            "https://cdn.prod.website-files.com/69a0a45220c8336fe957ccba/69a2aefb0c5c9128f0dd58b6_Brand%2007.webp",
+        LogoAlt: "Brand 07 company logo",
+    },
+    {
+        LogoImg:
+            "https://cdn.prod.website-files.com/69a0a45220c8336fe957ccba/69a2aefb0f49bca3c1742f2f_Brand%2003.webp",
+        LogoAlt: "Brand 03 company logo",
+    },
+    {
+        LogoImg:
+            "https://cdn.prod.website-files.com/69a0a45220c8336fe957ccba/69a2aefbf5f22f3b3bf85a8a_Brand%2001.webp",
+        LogoAlt: "Brand 01 company logo",
+    },
+    {
+        LogoImg:
+            "https://cdn.prod.website-files.com/69a0a45220c8336fe957ccba/69a2aefb9dd713edf13a284f_Brand%2002.webp",
+        LogoAlt: "Brand 02 company logo",
+    },
+    {
+        LogoImg:
+            "https://cdn.prod.website-files.com/69a0a45220c8336fe957ccba/69a2aefbeb9a99a07522dd00_Brand%2004.webp",
+        LogoAlt: "Brand 04 company logo",
+    },
+    {
+        LogoImg:
+            "https://cdn.prod.website-files.com/69a0a45220c8336fe957ccba/69a2aefbeb9a99a07522dd00_Brand%2004.webp",
+        LogoAlt: "Brand 04 company logo",
+    },
+    {
+        LogoImg:
+            "https://cdn.prod.website-files.com/69a0a45220c8336fe957ccba/69a2aefb0adf56b97802a9be_Brand%2006.webp",
+        LogoAlt: "Brand 06 company logo",
+    },
+    {
+        LogoImg:
+            "https://cdn.prod.website-files.com/69a0a45220c8336fe957ccba/69a2aefbf5f22f3b3bf85a8a_Brand%2001.webp",
+        LogoAlt: "Brand 01 company logo",
+    },
+    {
+        LogoImg:
+            "https://cdn.prod.website-files.com/69a0a45220c8336fe957ccba/69a2aefb9dd713edf13a284f_Brand%2002.webp",
+        LogoAlt: "Brand 02 company logo",
+    },
+];
+const AboutScnd = ({ styles, logosRef }) => {
+    return (
+        <section className={styles.AboutScnd}>
+            <div className={styles.container}>
+                <div className={styles.ScndContent}>
+                    <div className={`${styles.data} ${styles.flex} ${styles.ac} ${styles.sb}`}>
+                        <div className={`${styles.images} ${styles.flex} ${styles.sb}`}>
+                            <img className={styles.firstImg} src="https://images.pexels.com/photos/7693199/pexels-photo-7693199.jpeg?_gl=1*bmy9ia*_ga*MTk2NTY1NTAwMS4xNzgwMDc1OTcy*_ga_8JE65Q40S6*czE3ODE4OTUzNzgkbzEwJGcxJHQxNzgxODk2OTE5JGo2MCRsMCRoMA.." alt="" />
+                            <img className={styles.lastImg} src="https://images.pexels.com/photos/6814524/pexels-photo-6814524.jpeg?_gl=1*17y9psa*_ga*MTk2NTY1NTAwMS4xNzgwMDc1OTcy*_ga_8JE65Q40S6*czE3ODE4OTUzNzgkbzEwJGcxJHQxNzgxODk2ODkzJGoxJGwwJGgw" alt="" />
+                        </div>
+
+                        <div className={styles.numbers}>
+                            {stats.map((item, index) => (
+                                <div key={index}>
+                                    <p>{item.Result}</p>
+                                    <p>{item.Reason}</p>
+                                </div>
+                            ))}
+                        </div>
+                    </div>
+                </div>
+                <div className={styles.logoWrap}>
+                    <div className={styles.logoTrack} ref={logosRef}>
+                        {brands.map((brand, index) => (
+                            <img
+                                key={index}
+                                src={brand.LogoImg}
+                                alt={brand.LogoAlt}
+                            />
+                        ))}
+                    </div>
+                </div>
+            </div>
+        </section>
+    );
+};
+
+export default AboutScnd;
