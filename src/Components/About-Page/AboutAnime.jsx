2eedd2aa02666c7840a386b9354e2749b43d87e1
Guri <guristacks@gmail.com>
2026-06-20 15:36:35 +0530

    Add counter animation functionality to AboutAnime component

--- src/Components/About-Page/AboutAnime.jsx
+++ src/Components/About-Page/AboutAnime.jsx
@@ -3,7 +3,7 @@ import gsap from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
 import { useGSAP } from "@gsap/react";
 
-const AboutAnime = ({ blackRef, helloRef, logosRef }) => {
+const AboutAnime = ({ blackRef, helloRef, logosRef, counterRefs, sectionRef, testimonialsRef }) => {
   useGSAP(() => {
     let scrollY = 0;
     const preventScroll = (e) => {
@@ -63,6 +63,38 @@ const AboutAnime = ({ blackRef, helloRef, logosRef }) => {
     gsap.to(logosRef.current, {
       x: -1100,
       duration: 20,
+      ease: "none",
+      repeat: -1,
+      yoyo: true,
+    });
+
+
+    counterRefs.current.forEach((counter) => {
+      const endValue = Number(counter.dataset.value);
+      const prefix = counter.dataset.prefix || "";
+      const suffix = counter.dataset.suffix || "";
+      const obj = { value: 1 };
+      gsap.to(obj, {
+        value: endValue,
+        duration: 2,
+        ease: "power2.out",
+        scrollTrigger: {
+          trigger: sectionRef.current,
+          start: "top 75%",
+          toggleActions: "play none none none",
+        },
+        onUpdate: () => {
+          counter.textContent =
+            prefix +
+            Math.floor(obj.value) +
+            suffix;
+        },
+      });
+    });
+
+    gsap.to(testimonialsRef.current, {
+      x: -1675,
+      duration: 15,
       ease: "none",
       repeat: -1,
       yoyo: true,
