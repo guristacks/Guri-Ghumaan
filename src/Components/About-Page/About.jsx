2009bd9190c930b0d1502e4069dd58337207377b
Guri <guristacks@gmail.com>
2026-06-20 15:36:29 +0530

    Include counterRefs and sectionRef, and update AboutScnd and AboutAnime props

--- src/Components/About-Page/About.jsx
+++ src/Components/About-Page/About.jsx
@@ -6,11 +6,15 @@ import styles from "./AboutPage.module.css";
 import AboutTop from "./AboutTop";
 import AboutAnime from "./AboutAnime";
 import AboutScnd from "./AboutScnd";
+import Testimonials from "./Testimonials";
 
 const About = () => {
   const blackRef = useRef(null);
   const helloRef = useRef(null);
   const logosRef = useRef(null);
+  const counterRefs = useRef([]);
+  const sectionRef = useRef(null);
+  const testimonialsRef = useRef(null);
 
   const navLinks = [
     { name: "Home", path: "/" },
@@ -27,10 +31,23 @@ const About = () => {
       <Header navLinks={navLinks} isDark={true} />
       <main>
         <AboutTop blackRef={blackRef} helloRef={helloRef} styles={styles} />
-        <AboutScnd styles={styles} logosRef={logosRef} />
+        <AboutScnd
+          styles={styles}
+          logosRef={logosRef}
+          counterRefs={counterRefs}
+          sectionRef={sectionRef}
+        />
+        <Testimonials styles={styles} testimonialsRef={testimonialsRef} />
       </main>
       <Footer />
-      <AboutAnime blackRef={blackRef} helloRef={helloRef} logosRef={logosRef} />
+      <AboutAnime
+        blackRef={blackRef}
+        helloRef={helloRef}
+        logosRef={logosRef}
+        counterRefs={counterRefs}
+        sectionRef={sectionRef}
+        testimonialsRef={testimonialsRef}
+      />
     </>
   );
 };
