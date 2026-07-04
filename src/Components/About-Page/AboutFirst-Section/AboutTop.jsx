eae69efe83330de14fe4b6e842f4496a7e58a606
Guri Ghumaan <guristacks@gmail.com>
2026-07-04 02:02:36 -0700

    Add AboutTop component and associated styles for the About page

---
+++ src/Components/About-Page/AboutFirst-Section/AboutTop.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import styles from "./AboutTop.module.css";
+
+const AboutTop = ({ helloRef, blackRef }) => {
+    return (
+        <section className={styles.AboutTopSec}>
+            <div ref={blackRef} className={`${styles.black} ${styles.flex} ${styles.ac} ${styles.jc}`}>
+                <h2 ref={helloRef} className={styles.helloText}>• About</h2>
+            </div>
+            <div className={styles.container}>
+                <div className={styles.TopContent}>
+                    <h2>Crafting scalable web experiences. </h2>
+                    <div className={`${styles.TopBtm} ${styles.flex} ${styles.ac} ${styles.sb}`}>
+                        <p>Branding</p>
+                        <p>Design</p>
+                        <p>Development</p>
+                    </div>
+                </div>
+            </div>
+        </section>
+    )
+}
+
+export default AboutTop;
